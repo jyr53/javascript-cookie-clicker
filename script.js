cb3af5dd4b5ac82eb7f3cad0d061b7403383a684
jyr <jean-yves.rouseau@orange.fr>
2022-07-12 21:37:42 +0200

    work du 12/07

--- script.js
+++ script.js
@@ -4,62 +4,76 @@ let arrCout=[50,100,150,200,250,300,350,400,450,500,550,600,650];
 let autoTime=[0,0,0,1000,500,333,250,200,150]
 let  cout=0;
 let autoC=3;
+let limitBonus=5000;
 let nIntervId;
-function MAJAffichage(){
-    let piece=  document.getElementById('affichage');
-    piece.innerText=score;
+let valBonus=1;
+let limitT=30;
+let id;
+let element;
+let bloqBo=true;
+
+function MAJAffichage(id,element){ //fonction affichage
+    let piece=  document.getElementById(id);
+    piece.innerText=element;
 }
 
 function augmenter(){
-    score=score+fde;
-    MAJAffichage();
+    score=score+fde*valBonus;
+    MAJAffichage("affichage",score);
     controle();
 }
+
 function aleatoire(){
 if (score>arrCout[cout]){
     let arr = [1,2,3,4,5,6];
-    fde = arr[getRandomInt(6)];
-    faceDe();
+    fde = arr[Math.floor(Math.random() * 6)];
+    MAJAffichage("affichade",fde);
     score=score-arrCout[cout];
-    MAJAffichage();
+    MAJAffichage("affichage",score);
     cout++;
-    prixDe();
+    MAJAffichage("cout",arrCout[cout]);
 }}
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);}  
-
-function faceDe(){
+  
+function controle(){//permet de modifier l'affichage selon son id en css
     let piece=  document.getElementById('affichade');
-    piece.innerText=fde;
-    console.log(fde);
-}
-function controle(){
     if (score>arrCout[cout]){
-        let piece=  document.getElementById('affichade');
         piece.style.opacity= 1;
         piece.style.color = 'red';
         piece.style.backgroundColor = 'yellow';
 }
 else {
-        let piece=  document.getElementById('affichade');
-        piece.style.opacity=0.5;
-}
-}
-function prixDe(){
-    let piece=  document.getElementById('cout');
-    piece.innerText=arrCout[cout];
+        piece.style.opacity=0;
+}}
 
-}
-function autoClick(){
+function autoClick(){ //fonction 
   if (score>arrCout[autoC]){
          score=score-arrCout[autoC];
-            MAJAffichage();
+         MAJAffichage("affichage",score);
             nIntervId=null;
     if (!nIntervId) {
         nIntervId = setInterval(augmenter,autoTime[autoC]);}
-        prixauto();
+        MAJAffichage("coutAuTC",arrCout[autoC]);
         autoC++;
 }}
-function prixauto(){
-    let piece=  document.getElementById('coutAuTC');
-    piece.innerText=arrCout[autoC];}
+
+function bonus(){
+    if (score >limitBonus&&bloqBo){
+        valBonus=2;
+        bloqBo=false;
+        score=score-5000;
+        MAJAffichage("affichage",score);
+        tempoS=setInterval(tempsBonus,1000);
+}}
+
+function tempsBonus(){
+  limitT--
+  MAJAffichage("timeBonus",limitT);
+if (limitT==0)
+{
+ valBonus=1;
+ bloqBo=true;
+ clearInterval(tempoS);
+ limitT=30;
+ MAJAffichage("timeBonus",limitT);
+}}
+
